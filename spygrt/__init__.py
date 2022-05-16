819290ffefdb6b9f5e19cae02ed6884f9c693b28
Youssef Ben Bouchta <youssef.benbouchta@sydney.edu.au>
2022-05-16 15:16:02 +1000

    Update __init__.py

---
+++ spygrt/__init__.py
@@ -0,0 +1,7 @@
+"""
+SpyGRT, or Surface Guided Radiotherapy Toolkit in Python is a toolkit that allows fast development of novel
+surface guidance applications for radiotherapy.
+"""
+
+__author__ = 'Youssef Ben Bouchta'
+__version = '0.1.0'
